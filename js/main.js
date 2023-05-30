1849951bc996d2832c3c7e6837ccd98787a27ab0
Kylian Fraigne <93408369+Hbaysaru@users.noreply.github.com>
2023-05-30 15:49:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,56 +1,66 @@
-window.onscroll = function() {
-  var header = document.getElementById('header');
-  if (window.scrollY > 100) {
-    header.classList.add('fixed');
-  } else {
-    header.classList.remove('fixed');
-  }
-}
+window.addEventListener('scroll', function() {
+    var suns = document.querySelectorAll('.sunsvg');
+    var timelines = document.querySelectorAll('.timeline');
+    var texts = document.querySelectorAll('.text');
+    var screenHeight = window.innerHeight;
+  
+    suns.forEach(function(sun) {
+      var sunRect = sun.getBoundingClientRect();
+      var previousElement = sun.previousElementSibling;
+  
+      if (sunRect.top <= screenHeight / 2) {
+        sun.style.transition = 'all 1.5s';
+        sun.style.transform = 'rotate(180deg)';
+        sun.style.opacity = 1;
+        if (previousElement && previousElement.tagName === 'H2'){
+            previousElement.style.transition = 'all 1s';
+            previousElement.style.opacity = 1;
+        }
+      }
+    });
 
-function openModal(n) {
-  document.body.style.overflow = "hidden";
-  if (n === 1) {
-    document.getElementById('bigscreen1')
-    .classList.replace("bigscreen-none", "bigscreen");
-  }
-  if (n === 2) {
-    document.getElementById('bigscreen2')
-    .classList.replace("bigscreen-none", "bigscreen");
-  }
-  if (n === 3) {
-    document.getElementById('bigscreen3')
-    .classList.replace("bigscreen-none", "bigscreen");
-  }
-  if (n === 4) {
-    document.getElementById('bigscreen4')
-    .classList.replace("bigscreen-none", "bigscreen");
-  }
-  if (n === 5) {
-    document.getElementById('bigscreen5')
-    .classList.replace("bigscreen-none", "bigscreen");
-  }
-}
+    timelines.forEach(function(timeline) {
+      var timelineRect = timeline.getBoundingClientRect();
+      var timelineTop = timelineRect.top;
+    
+      var timelineHeight = screenHeight / 2 - timelineTop;
+    
+      if (timelineTop <= screenHeight / 2) {
+        timeline.style.height = timelineHeight + 'px';
+        timeline.style.opacity = 1;
+        timeline.style.transition = 'opacity 1s';
+      } else {
+        timeline.style.height = '0px';
+      }
+    });
 
-function closeModal(n) {
-  document.body.style.overflow = "auto";
-  if (n === 1) {
-    document.getElementById('bigscreen1')
-    .classList.replace("bigscreen", "bigscreen-none");
-  }
-  if (n === 2) {
-    document.getElementById('bigscreen2')
-    .classList.replace("bigscreen", "bigscreen-none");
-  }
-  if (n === 3) {
-    document.getElementById('bigscreen3')
-    .classList.replace("bigscreen", "bigscreen-none");
-  }
-  if (n === 4) {
-    document.getElementById('bigscreen4')
-    .classList.replace("bigscreen", "bigscreen-none");
-  }  
-  if (n === 5) {
-    document.getElementById('bigscreen5')
-    .classList.replace("bigscreen", "bigscreen-none");
-  } 
-}
+    var timelineA = document.querySelector('.timeline-a');
+    var timelineB = document.querySelector('.timeline-b');
+    var timelineC = document.querySelector('.timeline-c');
+  
+    var timelineARect = timelineA.getBoundingClientRect();
+  
+    if (timelineARect.top <= screenHeight / 2) {
+      timelineA.style.opacity = 1;
+      timelineB.style.opacity = 1;
+      timelineC.style.opacity = 1;
+    } else {
+      timelineA.style.opacity = 0;
+      timelineB.style.opacity = 0;
+      timelineC.style.opacity = 0;
+    }
+
+    texts.forEach(function(text) {
+      var textRect = text.getBoundingClientRect();
+  
+      if (textRect.top <= screenHeight / 2) {
+        if (text.classList.contains('js-small-block-text')){
+          text.style.right = 0;
+        } else {
+          text.style.left = 0;
+        }
+        text.style.transition = 'all 1s';
+        text.style.opacity = 1;
+      }
+    });
+  });
